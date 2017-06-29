c543e4012a62112db917677e0dfeecae502813d3
Donovan Lambert <d.lambert@insolus.com>
2017-06-29 12:10:43 +0200

    Add configuration files

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ module.exports = function (options = {}) {
         options = { targets: options };
     }
     const _options = Object.assign({}, options);
-    
+
     let targets = _options.targets;
     if (typeof targets === 'string') {
         targets = [targets];
     }
     delete _options.targets;
-    
+
     const _babelrc = babelrc(_options);
     if (!targets.length) {
         return _babelrc;
     }
-    
+
     _babelrc.presets = _babelrc.presets.map((preset) => {
         if (!Array.isArray(preset)) {
             preset = [preset];
@@ -26,12 +26,12 @@ module.exports = function (options = {}) {
         if (name !== 'env') {
             return preset;
         }
-        
+
         const _targets = Object.keys(config.targets || {});
         if (!_targets.length) {
             return preset;
         }
-        
+
         _targets.forEach((_target) => {
             if (targets.indexOf(_target) === -1) {
                 delete config.targets[_target];
@@ -40,6 +40,6 @@ module.exports = function (options = {}) {
 
         return [name, config];
     });
-    
+
     return _babelrc;
 };
